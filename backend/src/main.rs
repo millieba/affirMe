a37a0f37824303ca4548d052c49f77b1443685a5
millieba <millieba@hotmail.com>
2023-07-15 00:31:57 +0200

    #1 Database setup done. Made an endpoint that connects to the database and gets a random affirmation.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,27 +1,48 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use futures::stream::StreamExt;
+use mongodb::{
+    bson::{doc, Document},
+    options::ClientOptions,
+    Client,
+};
+use rand::seq::SliceRandom;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct Affirmation {
+    #[serde(rename = "Affirmation")]
+    text: String,
+    #[serde(rename = "Tag")]
+    tag: String,
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    println!("Received request with body: {}", req_body);
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[get("/affirmations/random")]
+async fn random_affirmation(client: web::Data<Client>) -> impl Responder {
+    let collection = client
+        .database("affirMe")
+        .collection::<Document>("affirmations");
+    let mut cursor = collection.find(None, None).await.unwrap();
+    let mut affirmations = Vec::<Affirmation>::new();
+    while let Some(Ok(document)) = cursor.next().await {
+        let affirmation = Affirmation {
+            text: document.get_str("Affirmation").unwrap().to_owned(),
+            tag: document.get_str("Tag").unwrap().to_owned(),
+        };
+        affirmations.push(affirmation);
+    }
+    let random_affirmation = affirmations.choose(&mut rand::thread_rng()).unwrap();
+    HttpResponse::Ok().json(random_affirmation)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let client_options = ClientOptions::parse("mongodb://localhost:27017")
+        .await
+        .unwrap();
+    let client = Client::with_options(client_options).unwrap();
+    HttpServer::new(move || {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .app_data(web::Data::new(client.clone()))
+            .service(random_affirmation)
     })
     .bind(("127.0.0.1", 8080))?
     .run()

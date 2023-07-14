c4444303f1be0f09cf01c056195c49897c872b05
millieba <millieba@hotmail.com>
2023-07-15 00:41:54 +0200

    #1 Added error handling.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,9 +9,7 @@ use rand::seq::SliceRandom;
 
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
 struct Affirmation {
-    #[serde(rename = "Affirmation")]
     text: String,
-    #[serde(rename = "Tag")]
     tag: String,
 }
 
@@ -20,15 +18,35 @@ async fn random_affirmation(client: web::Data<Client>) -> impl Responder {
     let collection = client
         .database("affirMe")
         .collection::<Document>("affirmations");
-    let mut cursor = collection.find(None, None).await.unwrap();
-    let mut affirmations = Vec::<Affirmation>::new();
-    while let Some(Ok(document)) = cursor.next().await {
-        let affirmation = Affirmation {
-            text: document.get_str("Affirmation").unwrap().to_owned(),
-            tag: document.get_str("Tag").unwrap().to_owned(),
-        };
-        affirmations.push(affirmation);
+
+    let affirmations = match collection.find(None, None).await {
+        Ok(cursor) => {
+            cursor
+                .filter_map(|result| async {
+                    match result {
+                        Ok(document) => Some(Affirmation {
+                            text: document.get_str("Affirmation").ok()?.to_owned(),
+                            tag: document.get_str("Tag").ok()?.to_owned(),
+                        }),
+                        Err(err) => {
+                            eprintln!("Error: {}", err);
+                            None
+                        }
+                    }
+                })
+                .collect::<Vec<Affirmation>>()
+                .await
+        }
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            return HttpResponse::InternalServerError().body(format!("Error: {}", err));
+        }
+    };
+
+    if affirmations.is_empty() {
+        return HttpResponse::InternalServerError().body("No affirmations found in the database");
     }
+
     let random_affirmation = affirmations.choose(&mut rand::thread_rng()).unwrap();
     HttpResponse::Ok().json(random_affirmation)
 }
@@ -37,8 +55,10 @@ async fn random_affirmation(client: web::Data<Client>) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     let client_options = ClientOptions::parse("mongodb://localhost:27017")
         .await
-        .unwrap();
-    let client = Client::with_options(client_options).unwrap();
+        .expect("Failed to parse MongoDB client options");
+
+    let client = Client::with_options(client_options).expect("Failed to create MongoDB client");
+
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(client.clone()))

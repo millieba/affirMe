b94190de2ef993b17ac0b3375844f688d9efa40e
millieba <millieba@hotmail.com>
2023-08-06 11:10:06 +0200

    #10 Setup Svelte with Tailwind

---
+++ frontend/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<div class="bg-red-700">
+  <Counter />
+</div>
+
+<style global lang="postcss">
+  @tailwind utilities;
+  @tailwind components;
+  @tailwind base;
+</style>
